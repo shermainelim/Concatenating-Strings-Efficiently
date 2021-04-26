3dde97b882d4e7c854a98b93713d30ee2314af7f
shermaine <shermaine.lim.si.hui@gmail.com>
2021-04-26 22:09:59 +0800

    Commit 1

---
+++ Concat  vs StringBuilder/Concat  vs StringBuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace Concat__vs_StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String Concat
+            DateTime start = DateTime.Now;
+            string x = "";
+            for (int i = 0; i < 100000; i++)
+            {
+                x += "!";
+            }
+            DateTime end = DateTime.Now;
+            Console.WriteLine("Time taken for String Concat: {0}", end - start);
+
+
+            // String Builder
+            DateTime start1 = DateTime.Now;
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 100000; i++)
+            {
+                builder.Append("!");
+            }
+            string x1 = builder.ToString();
+            DateTime end1 = DateTime.Now;
+            Console.WriteLine("Time taken for String Builder: {0}", end1 - start1);
+        }
+    }
+}
